375175c33564a7d528fabb844a89cdc707101310
oldaniMarcos <139399407+oldaniMarcos@users.noreply.github.com>
2024-09-26 12:39:36 -0300

    Add components, styles, routes
    Added home, header, page-not-found components, styles, basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component.js';
+import { AppComponent } from './app.component.js';
+import { HomeComponent } from './components/home/home.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'page-not-found', component: PageNotFoundComponent },
+
+  // Wildcard route, selecciona esta ruta si el URL no coincide con ningun path
+  { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' }
+
+];
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Options } from '../../types.js';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  // T es un tipo generico, por lo que esta funcion puede manejar cualquier tipo de dato
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>
+  }
+
+  // B tambien es un tipo generico, hacer esto es mejor que body: any ya que con any puedo mandar cualquier cosa y me lo va a tomar
+  post<T, B>(url: string, body: B, options: Options): Observable<T> {
+    return this.httpClient.post<T>(url, body, options) as Observable<T>
+  }
+
+  put<T, B>(url: string, body: B, options: Options): Observable<T> {
+    return this.httpClient.put<T>(url, body, options) as Observable<T>
+  }
+
+  /*
+    post<T>(url: string, body: any, options: Options): Observable<T> {
+    return this.httpClient.post<T>(url, body, options) as Observable<T>
+  }
+
+  put<T>(url: string, body: any, options: Options): Observable<T> {
+    return this.httpClient.put<T>(url, body, options) as Observable<T>
+  }
+  */
+
+  delete<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.delete<T>(url, options) as Observable<T>
+  }
+}
